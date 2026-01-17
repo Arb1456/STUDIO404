267f1226d2dd80c331724bb1a24ad2564c452269
Andre Beaudin <andrebeaudin@Andres-MBP.ht.home>
2026-01-16 20:09:19 -0500

    Change scroll animation duration to 0.8s

--- src/components/ui/Reveal.tsx
+++ src/components/ui/Reveal.tsx
@@ -28,7 +28,7 @@ export const Reveal: React.FC<RevealProps> = ({
                 }}
                 initial="hidden"
                 animate={isInView ? "visible" : "hidden"}
-                transition={{ duration: 1.4, delay: delay, ease: [0.22, 1, 0.36, 1] }}
+                transition={{ duration: 0.8, delay: delay, ease: [0.22, 1, 0.36, 1] }}
             >
                 {children}
             </motion.div>
